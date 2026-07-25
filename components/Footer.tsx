655bc7b23e45afd028203b9d6d264e4fd89bb661
Fundora Capital <amimezawi@gmail.com>
2026-07-25 21:25:06 +0700

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="py-10 text-center text-gray-500 border-t border-zinc-800">
+
+      © 2026 Fundora Capital
+
+    </footer>
+  );
+}
