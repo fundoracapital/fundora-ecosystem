b7ce37787ef92db84fd91a24b225d7bc80eb9e49
Fundora Capital <amimezawi@gmail.com>
2026-07-25 21:25:52 +0700

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = "Fundora Capital";
+
+export const TAGLINE =
+"Pass the evaluation. Get funded. Withdraw in SOL.";
+
+export const DESCRIPTION =
+"The decentralized prop firm for Solana meme traders.";
